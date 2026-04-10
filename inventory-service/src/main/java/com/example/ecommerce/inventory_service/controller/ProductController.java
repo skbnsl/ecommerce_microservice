7da037040029a1e58288fe69085153aaf2b0eb76
sagarbansal <bansalsagar13@gmail.com>
2026-04-11 01:39:35 +0530

    create api gateway microservice and working with apis

--- inventory-service/src/main/java/com/example/ecommerce/inventory_service/controller/ProductController.java
+++ inventory-service/src/main/java/com/example/ecommerce/inventory_service/controller/ProductController.java
@@ -32,7 +32,7 @@ public class ProductController {
         ServiceInstance orderService = discoveryClient.getInstances("order-service").getFirst();
 
         String response = restClient.get()
-                .uri(orderService.getUri() + "/api/v1/orders/helloOrders")
+                .uri(orderService.getUri() + "/orders/core/helloOrders")
                 .retrieve()
                 .body(String.class);
         return response;
--- order-service/src/main/java/com/example/ecommerce/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/example/ecommerce/order_service/controller/OrdersController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/orders")
+@RequestMapping("/core")
 @Slf4j
 public class OrdersController {
 
